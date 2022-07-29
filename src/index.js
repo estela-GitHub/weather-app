1d53189ecd124108ca98663235090170c6d66749
estela-GitHub <106884854+estela-GitHub@users.noreply.github.com>
2022-07-29 19:58:38 +0200

    change layout

--- src/index.js
+++ src/index.js
@@ -25,20 +25,20 @@ if (currentMinutes < 10) {
 }
 
 function displayForecast() {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
                    <div class="col-2">
-                    <div class="weather-forecast-date">${day}</div>
+                    <div class="weather-forecast-date">${forecastDay.dt}</div>
                     <i class="fa-solid fa-sun"></i></li>
                     <div class="weather-forecast-temperatures">
-                    <span class="weather-forecast-temperature-max">18° </span>
-                    <span class="weather-forecast-temperature-min">12° </span>
+                    <span class="weather-forecast-temperature-max">{forecastDay.temp.max}° </span>
+                    <span class="weather-forecast-temperature-min">{forecastDay.temp.min}° </span>
                   </div>
                 </div>
   `;
@@ -47,6 +47,12 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "339b9c7988721b2d1d0214d41baf8b17";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid={apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -70,6 +76,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -114,4 +122,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Barcelona");
-displayForecast();
